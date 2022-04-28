44e906e2573acad0b89b6358e56ec26ed2dbdba0
eleanna <eleannaval1996@gmail.com>
2022-04-28 11:36:30 +0300

    last changes in lab1

--- lab1/scripts/make_fsts.sh
+++ lab1/scripts/make_fsts.sh
@@ -18,7 +18,7 @@ fstdraw --isymbols=../vocab/chars.syms --osymbols=../vocab/chars.syms ../fsts/su
 cat ../vocab/words.syms | python3 makelex.py > ../fsts/V.fst
 fstcompile --isymbols=../vocab/chars.syms --osymbols=../vocab/words.syms ../fsts/V.fst ../fsts/V.binfst
 # sub acceptor
-head -10 ../vocab/words.syms | python3 makelex.py > ../fsts/sub_V.fst
+head -20 ../vocab/words.syms | python3 makelex.py > ../fsts/sub_V.fst
 fstcompile --isymbols=../vocab/chars.syms --osymbols=../vocab/words.syms ../fsts/sub_V.fst ../fsts/sub_V.binfst
 # create W acceptor
 fstcompile --isymbols=../vocab/words.syms --osymbols=../vocab/words.syms ../fsts/W.fst ../fsts/W.binfst
@@ -29,15 +29,24 @@ fstcompile --isymbols=../vocab/words.syms --osymbols=../vocab/words.syms ../fsts
 fstdraw --isymbols=../vocab/chars.syms --osymbols=../vocab/words.syms ../fsts/sub_V.binfst | dot -Tpng > ../img/sub_V.png
 fstdraw --isymbols=../vocab/words.syms --osymbols=../vocab/words.syms ../fsts/sub_W.binfst | dot -Tpng > ../img/sub_W.png
 
-# optimization 
+## General optimization 
 fstrmepsilon ../fsts/V.binfst| fstdeterminize | fstminimize > ../fsts/V_opt.binfst
 fstrmepsilon ../fsts/sub_V.binfst| fstdeterminize | fstminimize >../fsts/sub_V_opt.binfst
+# rm epsilon opt
+fstrmepsilon ../fsts/sub_V.binfst >../fsts/V_opt_rmepsilon.binfst
+# determinize opt
+fstdeterminize ../fsts/sub_V.binfst >../fsts/V_opt_determinize.binfst
+# minimize opt
+fstminimize ../fsts/sub_V.binfst >../fsts/V_opt_minimize.binfst
 
 fstrmepsilon ../fsts/W.binfst| fstdeterminize | fstminimize > ../fsts/W_opt.binfst
 fstrmepsilon ../fsts/sub_W.binfst| fstdeterminize | fstminimize >../fsts/sub_W_opt.binfst
 
 # draw optimized graph
 fstdraw --isymbols=../vocab/chars.syms --osymbols=../vocab/words.syms ../fsts/sub_V_opt.binfst | dot -Tpng > ../img/sub_V_opt.png
+fstdraw --isymbols=../vocab/chars.syms --osymbols=../vocab/words.syms ../fsts/V_opt_rmepsilon.binfst | dot -Tpng > ../img/V_opt_rmepsilon.png
+fstdraw --isymbols=../vocab/chars.syms --osymbols=../vocab/words.syms ../fsts/V_opt_determinize.binfst | dot -Tpng > ../img/V_opt_determinize.png
+fstdraw --isymbols=../vocab/chars.syms --osymbols=../vocab/words.syms ../fsts/V_opt_minimize.binfst | dot -Tpng > ../img/V_opt_minimize.png
 
 # sort L and V fst inputs
 # fstarcsort --sort_type=olabel ../fsts/L.binfst ../fsts/L_sorted.binfst
